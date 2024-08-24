421e390fecd4c0a3aa03be2fca88f5e28a2e9de4
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2024-08-25 10:37:33 +1200

    Creating service method to add new user

--- backend/src/api/user/user.service.ts
+++ backend/src/api/user/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { User } from './user.interface';
+import { UserDTO } from './interfaces/user.dto';
 import { users } from 'src/dummydata';
+import { CreateUserDTO } from './interfaces/create-user.dto';
 
 @Injectable()
 export class UserService {
@@ -8,13 +9,24 @@ export class UserService {
     return { msg: 'Hi Everyone!!!' };
   }
 
-  getUser(id: number): User[] {
-    const found: User[] = users.map((user: User) => {
+  getUser(id: number): UserDTO[] {
+    const found: UserDTO[] = users.map((user: UserDTO) => {
       if (user.ID == id) {
         return user;
       }
     });
 
     return found;
+  }
+
+  createUser(user: CreateUserDTO): number {
+    const newUser: UserDTO = {
+      ID: users.length + 1,
+      Name: user.Name,
+      Email: user.Email,
+      Password: user.Password,
+    };
+    users.push(newUser);
+    return newUser.ID;
   }
 }
