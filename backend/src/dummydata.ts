9f286329fafea238a7b8ef4af96dc748e9429a4a
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2024-08-25 10:09:51 +1200

    Adding dummy data file

---
+++ backend/src/dummydata.ts
@@ -0,0 +1,10 @@
+import { User } from './api/user/user.interface';
+
+export const users: User[] = [
+  {
+    ID: 1,
+    Name: 'John',
+    Password: '1234',
+    Email: 'john@test.com',
+  },
+];
