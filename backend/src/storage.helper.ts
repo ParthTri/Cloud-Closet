2b19a46f5f0730fa729320d856cd3f40af96a340
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2024-08-27 16:16:03 +1200

    Reformatting file

--- backend/src/storage.helper.ts
+++ backend/src/storage.helper.ts
@@ -1,47 +1,53 @@
 import { Injectable } from '@nestjs/common';
-const { BlobServiceClient } = require("@azure/storage-blob");
-require("dotenv").config();
+const { BlobServiceClient } = require('@azure/storage-blob');
 
-const AZURE_STORAGE_CONNECTION_STRING = process.env["storage_connection_string"];
+const AZURE_STORAGE_CONNECTION_STRING =
+  process.env['STORAGE_CONNECTION_STRING'];
 
 // Create the BlobServiceClient object with connection string
 const blobServiceClient = BlobServiceClient.fromConnectionString(
-  AZURE_STORAGE_CONNECTION_STRING
+  AZURE_STORAGE_CONNECTION_STRING,
 );
 
 @Injectable()
 export class StorageHelper {
-    async uploadImage(image : string | Blob, containerName : string, blobName: string): Promise<string> {
-        // Get a block blob client
-        const containerClient = blobServiceClient.getContainerClient(containerName);
-        const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+  async uploadImage(
+    image: string | Blob,
+    containerName: string,
+    blobName: string,
+  ): Promise<string> {
+    // Get a block blob client
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
 
-        // Create blobOptions
-        var fileType = blobName.split('.')[1];
-        if (fileType == "jpg") {
-            fileType = "jpeg;"
-        }
-        const blobOptions = { blobHTTPHeaders: { blobContentType: `image/${fileType}` } };
-    
-        // Upload data to the blob
-        if (image instanceof Blob) {
-            const arrayBuffer = await image.arrayBuffer();
-            const buffer = Buffer.from(arrayBuffer);
-            await blockBlobClient.uploadData(buffer, blobOptions);
-        } else {
-            let matchesBlobImg = image.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
-            let imgBuffer = Buffer.from(matchesBlobImg[2], 'base64');
-            await blockBlobClient.uploadData(imgBuffer, blobOptions);
-        }
+    // Create blobOptions
+    var fileType = blobName.split('.')[1];
+    if (fileType == 'jpg') {
+      fileType = 'jpeg;';
+    }
+    const blobOptions = {
+      blobHTTPHeaders: { blobContentType: `image/${fileType}` },
+    };
 
-        return blockBlobClient.url;
+    // Upload data to the blob
+    if (image instanceof Blob) {
+      const arrayBuffer = await image.arrayBuffer();
+      const buffer = Buffer.from(arrayBuffer);
+      await blockBlobClient.uploadData(buffer, blobOptions);
+    } else {
+      let matchesBlobImg = image.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+      let imgBuffer = Buffer.from(matchesBlobImg[2], 'base64');
+      await blockBlobClient.uploadData(imgBuffer, blobOptions);
     }
 
-    async deleteImage(imageName, containerName) {
-        // Get a block blob client
-        const containerClient = blobServiceClient.getContainerClient(containerName);
-        const blockBlobClient = containerClient.getBlockBlobClient(imageName);
+    return blockBlobClient.url;
+  }
 
-        await blockBlobClient.delete();
-    }
+  async deleteImage(imageName, containerName) {
+    // Get a block blob client
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+    const blockBlobClient = containerClient.getBlockBlobClient(imageName);
+
+    await blockBlobClient.delete();
+  }
 }
