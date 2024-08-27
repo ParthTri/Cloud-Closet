3541b5b10a5c993a427b6e9cda9088aacac318c4
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2024-08-27 22:24:39 +1200

    Registering entities for image module

--- backend/src/api/image/image.module.ts
+++ backend/src/api/image/image.module.ts
@@ -5,9 +5,10 @@ import { ImageController } from './image.controller';
 import { ImageService } from './image.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Image } from './image.entity';
+import { User } from '../user/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Image])],
+  imports: [TypeOrmModule.forFeature([Image, User])],
   controllers: [ImageController],
   providers: [DatabaseHelper, StorageHelper, ImageService],
 })
