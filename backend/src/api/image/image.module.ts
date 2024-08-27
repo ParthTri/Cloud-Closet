b8c1ab0ffc7f64023a8ab8dc137391caabd7639c
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2024-08-28 00:05:40 +1200

    Adding Category repository to image module

--- backend/src/api/image/image.module.ts
+++ backend/src/api/image/image.module.ts
@@ -6,9 +6,10 @@ import { ImageService } from './image.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Image } from './image.entity';
 import { User } from '../user/user.entity';
+import { Category } from '../category/category.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Image, User])],
+  imports: [TypeOrmModule.forFeature([Image, User, Category])],
   controllers: [ImageController],
   providers: [DatabaseHelper, StorageHelper, ImageService],
 })
