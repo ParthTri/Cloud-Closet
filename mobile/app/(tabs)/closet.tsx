cf3bc2180c40be18ad68d40cc236f1f33fe07d98
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2024-09-07 16:20:07 +1200

    Creating basic closet page to render images as a list of text

--- mobile/app/(tabs)/closet.tsx
+++ mobile/app/(tabs)/closet.tsx
@@ -1,1 +1,41 @@
-export default function Closet() {}
+import * as SecureStore from "expo-secure-store";
+import { useEffect, useState } from "react";
+import { View, Text } from "react-native";
+
+function getValueFor(key: string): string | null {
+	return SecureStore.getItem(key);
+}
+
+async function getUserItems(userID: string | null): Promise<any[]> {
+	if (userID == null) {
+		return [{}];
+	}
+	const data = await fetch(`http://192.168.1.36:3000/api/image/${userID}`, {
+		method: "GET",
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+		},
+	});
+	const json = await data.json();
+	console.log(json);
+	return json;
+}
+
+export default function Closet() {
+	const userID: string | null = getValueFor("userID");
+	console.log(userID);
+	const [items, setItems] = useState<any[]>([{}]);
+	useEffect(() => {
+		getUserItems(userID).then((x) => setItems(x));
+		console.log(items);
+	}, []);
+
+	return (
+		<View>
+			{items.map((element) => (
+				<Text key={element.imageID}>{element["imageID"]}</Text>
+			))}
+		</View>
+	);
+}
