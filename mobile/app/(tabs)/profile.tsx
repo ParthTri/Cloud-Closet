6a60e82b64938f817f3a1da2541a385e349b2ba8
Frank <mrf9226@autuni.ac.nz>
2024-09-11 12:50:24 +0800

    I wrote the code in the wrong place.

--- mobile/app/(tabs)/profile.tsx
+++ mobile/app/(tabs)/profile.tsx
@@ -1,1 +1,25 @@
-export default function Profile() {}
+import React from 'react';
+import { View, Button } from 'react-native';
+import { useNavigation, NavigationProp } from '@react-navigation/native';
+// 导航其他页面创建按钮
+
+type RootStackParamList = {
+  Profile: undefined;
+  Welcome: undefined;
+};
+
+export default function Profile() {
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+
+  const goToWelcome = () => {
+    // 导航到欢迎界面的逻辑
+    navigation.navigate('Welcome');
+  };
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      {/* I created a button that returns */}
+      <Button title="log out" onPress={goToWelcome}  color="red"/>
+    </View>
+  );
+}
