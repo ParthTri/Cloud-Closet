0734f69767e55243200813ab635ef7125313e234
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2024-09-18 08:28:21 +1200

    Adding structure for profile page with dummy text

--- mobile/app/(tabs)/profile.tsx
+++ mobile/app/(tabs)/profile.tsx
@@ -1,18 +1,55 @@
-import React from 'react';
-import { View, Button } from 'react-native';
-import { useRouter } from 'expo-router';
+import React from "react";
+import { View, Pressable, Text, Image, StyleSheet } from "react-native";
+import { useRouter } from "expo-router";
 
 export default function Profile() {
-  const router = useRouter();
+	const router = useRouter();
 
-  const goToHome = () => {
-    
-    router.push('../auth/signin');
-  };
+	const goToHome = () => {
+		router.push("../auth/signin");
+	};
 
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Button title="Log out" onPress={goToHome} color="red" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			{/* Overview */}
+			<View style={styles.profileOverview}>
+				<Image
+					source={require("@/assets/images/profile.svg")}
+					style={styles.profileImage}
+				/>
+				<Text style={{ fontSize: 18 }}>
+					youremail@domain.com | +64 234 567 8
+				</Text>
+			</View>
+
+			<View style={styles.card}>
+				<Text style={styles.cardItems}>Edit profile information</Text>
+				<Text style={styles.cardItems}>Notifications</Text>
+				<Text style={styles.cardItems}>Language</Text>
+			</View>
+
+			<View style={styles.card}>
+				<Text style={styles.cardItems}>Security</Text>
+				<Text style={styles.cardItems}>Theme</Text>
+			</View>
+
+			<View style={styles.card}>
+				<Text style={styles.cardItems}>Help & Support</Text>
+				<Text style={styles.cardItems}>Contact us</Text>
+				<Text style={styles.cardItems}>Privacy policy</Text>
+			</View>
+
+			<Pressable onPressIn={goToHome} style={styles.logout}>
+				<Text
+					style={{
+						fontWeight: "bold",
+						fontSize: 32,
+					}}
+				>
+					LOGOUT
+				</Text>
+			</Pressable>
+		</View>
+	);
 }
+
