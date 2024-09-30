c084108cde8f53bdcbe27631d6270ab88b9ef437
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2024-10-01 11:20:25 +1300

    Disabling category

--- backend/src/api/category/category.module.ts
+++ backend/src/api/category/category.module.ts
@@ -1,14 +1,14 @@
-import { Module } from '@nestjs/common';
-import { CategoryController } from './category.controller';
-import { DatabaseHelper } from 'src/database.helper';
-import { CategoryService } from './category.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Category } from './category.entity';
-import { User } from '../user/user.entity';
+// import { Module } from '@nestjs/common';
+// import { CategoryController } from './category.controller';
+// import { DatabaseHelper } from 'src/database.helper';
+// import { CategoryService } from './category.service';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// // import { Category } from './category.entity';
+// // import { User } from '../user/user.entity';
 
-@Module({
-  imports: [TypeOrmModule.forFeature([Category, User])],
-  controllers: [CategoryController],
-  providers: [DatabaseHelper, CategoryService],
-})
-export class CategoryModule {}
+// @Module({
+//   imports: [TypeOrmModule.forFeature([])],
+//   controllers: [CategoryController],
+//   providers: [DatabaseHelper, CategoryService],
+// })
+// export class CategoryModule {}
