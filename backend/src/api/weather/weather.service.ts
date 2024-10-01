bc4386604b41e38c0078660d8ab8914295492faf
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2024-10-02 12:34:54 +1300

    Creating service method to return local weather data in WeatherDTO format

--- backend/src/api/weather/weather.service.ts
+++ backend/src/api/weather/weather.service.ts
@@ -1,10 +1,34 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { SupabaseProvider } from 'src/supabase/supabase.service';
+import { GetWeatherDTO, WeatherDTO } from './interface/weather.dto';
+import axios from 'axios';
+const URL_BUILDER = (lat: number, long: number): string =>
+  `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${process.env.OPEN_WEATHER_MAP}`;
 
 @Injectable()
 export class WeatherService {
-  constructor(
-    @Inject(SupabaseProvider)
-    private supa: SupabaseProvider,
-  ) {}
+  constructor(private supa: SupabaseProvider) {}
+
+  async getWeatherData(locationData: GetWeatherDTO): Promise<any> {
+    let data: WeatherDTO;
+
+    try {
+      const responses = await axios.get(
+        URL_BUILDER(locationData.latitude, locationData.longitude),
+      );
+
+      console.log(responses.data);
+
+      data = {
+        weather: responses.data['weather'][0]['main'],
+        temperature: responses.data['main']['temp'],
+        location: responses.data['name'],
+      };
+    } catch (error) {
+      console.log(error);
+      return { data: null, error: error };
+    }
+
+    return { data: data, error: null };
+  }
 }
