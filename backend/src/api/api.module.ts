715316f60e75e293707aadd23f207e790bab6bf6
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2024-10-02 17:58:08 +1300

    Re-adding category module into API module

--- backend/src/api/api.module.ts
+++ backend/src/api/api.module.ts
@@ -3,10 +3,10 @@ import { ApiController } from './api.controller';
 import { UserModule } from './user/user.module';
 import { ImageModule } from './image/image.module';
 import { WeatherModule } from './weather/weather.module';
-// import { CategoryModule } from './category/category.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [UserModule, ImageModule, WeatherModule],
+  imports: [UserModule, ImageModule, CategoryModule, WeatherModule],
   controllers: [ApiController],
   providers: [],
 })
