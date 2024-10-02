027f24ad016b66d08ac111e0330aca2aa6674c1e
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2024-10-02 17:56:14 +1300

    Removing Database and storage helper import

--- backend/src/api/image/image.module.ts
+++ backend/src/api/image/image.module.ts
@@ -1,17 +1,11 @@
 import { Module } from '@nestjs/common';
-import { DatabaseHelper } from '../../database.helper';
-import { StorageHelper } from '../../storage.helper';
 import { ImageController } from './image.controller';
 import { ImageService } from './image.service';
-// import { TypeOrmModule } from '@nestjs/typeorm';
 import { SupabaseModule } from 'src/supabase/supabase.module';
-// import { Image } from './image.entity';
-// import { User } from '../user/user.entity';
-// import { Category } from '../category/category.entity';
 
 @Module({
   imports: [SupabaseModule],
   controllers: [ImageController],
-  providers: [DatabaseHelper, StorageHelper, ImageService],
+  providers: [ImageService],
 })
 export class ImageModule {}
