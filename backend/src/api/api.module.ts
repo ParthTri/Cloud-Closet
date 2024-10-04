d2d81678c898721ab3bc85d6b640a41e71d11f89
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2024-10-04 16:58:09 +1300

    Updating imports

--- backend/src/api/api.module.ts
+++ backend/src/api/api.module.ts
@@ -4,9 +4,16 @@ import { UserModule } from './user/user.module';
 import { ImageModule } from './image/image.module';
 import { WeatherModule } from './weather/weather.module';
 import { CategoryModule } from './category/category.module';
+import { GenOutfitModule } from './genOutfit/genOutfit.module';
 
 @Module({
-  imports: [UserModule, ImageModule, CategoryModule, WeatherModule],
+  imports: [
+    UserModule,
+    ImageModule,
+    CategoryModule,
+    WeatherModule,
+    GenOutfitModule,
+  ],
   controllers: [ApiController],
   providers: [],
 })
