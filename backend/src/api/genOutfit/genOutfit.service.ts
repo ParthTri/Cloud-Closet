54e968e414c0ba421e29d9a8101458eda5f06126
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2024-10-06 18:15:05 +1300

    Updating genOutfit service to also generate clothes for warm weather

--- backend/src/api/genOutfit/genOutfit.service.ts
+++ backend/src/api/genOutfit/genOutfit.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { WeatherService } from '../weather/weather.service';
-import { GenOutfitDTO } from './interface/genOutfit.dto';
+import { GenOutfitDTO, OutfitType } from './interface/genOutfit.dto';
 import { SupabaseProvider } from 'src/supabase/supabase.service';
 
 @Injectable()
@@ -12,61 +12,68 @@ export class GenOutfitService {
   ) {}
 
   async generateOutft(req: GenOutfitDTO) {
-    const weatherInfo = await this.weatherService.getWeatherData({
-      latitude: req.latitude,
-      longitude: req.longitude,
-    });
+    // const weatherInfo = await this.weatherService.getWeatherData({
+    //   latitude: req.latitude,
+    //   longitude: req.longitude,
+    // });
+
+    const weatherInfo = {
+      data: {
+        temperature: 18,
+      },
+    };
 
     // Set the search tag
-    const searchTag = weatherInfo.data.temperature <= 16 ? 'cool' : 'hot';
+    const searchTag = weatherInfo.data.temperature <= 16 ? 'cool' : 'warm';
 
     // Check if temperature is below 16 degrees C
     // If so -> look for item tags that are cool
     // else hot
     const outfit = [];
 
-    if (weatherInfo.data.temperature < 18) {
-      const { data, error } = await this.supa
-        .getClient()
-        .from('Image')
-        .select(
-          `
+    const { data, error } = await this.supa
+      .getClient()
+      .from('Image')
+      .select(
+        `
+        imageId,
+        userId,
+        processedUrl,
+        ImageCategory (
           imageId,
-          userId,
-          processedUrl,
-          ImageCategory (
-            imageId,
-            categoryId,
-            ItemCategory (
-              name
-            )
+          categoryId,
+          ItemCategory (
+            name
           )
-          `,
         )
-        .eq('userId', req.userId);
+        `,
+      )
+      .eq('userId', req.userId);
 
-      // Filtering responses to match the search tag in the image category
-      data.forEach((val) => {
-        val.ImageCategory.forEach((imageCat) => {
-          if (imageCat.ItemCategory['name'] == searchTag) {
-            outfit.push(val);
-          }
-        });
-      });
+    if (error) {
+      return error;
     }
+
+    // Filtering responses to match the search tag in the image category
+    data.forEach((val) => {
+      val.ImageCategory.forEach((imageCat) => {
+        if (imageCat.ItemCategory['name'] == searchTag) {
+          // console.log(imageCat);
+          outfit.push(val);
+        }
+      });
+    });
 
     const payload = [];
     let TOP = false;
     let BOTTOMS = false;
     let JACKET = false;
     // let DRESS = false;
-    const FORMAL = Math.random() < 0.5;
-    console.log(FORMAL);
     outfit.forEach((item) => {
       if (item['ImageCategory'].length > 1) {
         item['ImageCategory'].forEach((cat) => {
           const name: string = cat['ItemCategory']['name'];
-          if (FORMAL) {
+          if (req.type == OutfitType.FORMAL) {
             if (!TOP && name == 'shirt') {
               payload.push(item);
               TOP = true;
@@ -81,7 +88,7 @@ export class GenOutfitService {
             if (!TOP && name == 't-shirt') {
               payload.push(item);
               TOP = true;
-            } else if (!BOTTOMS && name == 'pants') {
+            } else if (!BOTTOMS && (name == 'pants' || name == 'shorts')) {
               payload.push(item);
               BOTTOMS = true;
             } else if (!JACKET && name == 'down jacket') {
@@ -92,6 +99,7 @@ export class GenOutfitService {
         });
       }
     });
+
     return payload;
   }
 }
