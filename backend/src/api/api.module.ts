2c5af3fd1326c170c144502cf3515b789ba24883
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2024-10-09 17:51:39 +1300

    Updating api module to include generate outfit module

--- backend/src/api/api.module.ts
+++ backend/src/api/api.module.ts
@@ -5,11 +5,21 @@ import { ImageModule } from './image/image.module';
 import { WeatherModule } from './weather/weather.module';
 import { CategoryModule } from './category/category.module';
 import { OutfitCategoryModule } from './outfitCategory/outfitCategory.module';
-import {OutfitModule} from './outfit/outfit.module';
+import { OutfitModule } from './outfit/outfit.module';
 import { SharedModule } from './sharedModule';
+import { GenOutfitModule } from './genOutfit/genOutfit.module';
 
 @Module({
-  imports: [SharedModule, UserModule, ImageModule, CategoryModule, WeatherModule, OutfitCategoryModule, OutfitModule],
+  imports: [
+    SharedModule,
+    UserModule,
+    ImageModule,
+    CategoryModule,
+    WeatherModule,
+    OutfitCategoryModule,
+    OutfitModule,
+    GenOutfitModule,
+  ],
   controllers: [ApiController],
   providers: [],
 })
