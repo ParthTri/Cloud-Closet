8d5025358c6ec66aa9a777d09f526d65fceb9794
ariiii28 <131920083+ariiii28@users.noreply.github.com>
2024-10-15 11:03:29 +1300

    log out button fixed

--- mobile/app/(tabs)/profile.tsx
+++ mobile/app/(tabs)/profile.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import { 
+import {
   SafeAreaView,
   ScrollView,
   TouchableOpacity,
-  View, 
-  Text, 
-  Switch, 
-  StyleSheet
+  View,
+  Text,
+  Switch,
+  StyleSheet,
 } from "react-native";
 import { useRouter } from "expo-router";
 import AntDesign from "@expo/vector-icons/AntDesign";
@@ -20,19 +20,15 @@ export default function Profile() {
   });
 
   const goToHome = () => {
-    router.push("/(tabs)");
+    router.push("../auth/signin");
   };
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#f8f8f8" }}>
       {/* Header */}
       <View style={styles.header}>
-        <TouchableOpacity
-          onPress={goToHome}
-          style={styles.headerAction}
-        >
+        <TouchableOpacity onPress={goToHome} style={styles.headerAction}>
           <AntDesign name="arrowleft" size={24} color="#000" />
-
         </TouchableOpacity>
         <Text numberOfLines={1} style={styles.headerTitle}>
           Profile
@@ -106,13 +102,13 @@ export default function Profile() {
           <View style={styles.sectionBody}>
             <TouchableOpacity style={styles.row}>
               <Text style={styles.rowLabel}>Help & Support</Text>
-			  <View style={styles.rowSpacer} />
+              <View style={styles.rowSpacer} />
               <AntDesign name="right" size={19} color="#bcbcbc" />
             </TouchableOpacity>
 
             <TouchableOpacity style={styles.row}>
               <Text style={styles.rowLabel}>Privacy Policy</Text>
-			  <View style={styles.rowSpacer} />
+              <View style={styles.rowSpacer} />
               <AntDesign name="right" size={19} color="#bcbcbc" />
             </TouchableOpacity>
           </View>
@@ -230,7 +226,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "flex-start",
     paddingRight: 12,
-	paddingLeft: 12,
+    paddingLeft: 12,
   },
   rowLabel: {
     fontSize: 16,
