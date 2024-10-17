cd55c7154a61b6a3c5f4d5368ebc098169deda41
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2024-10-17 15:00:56 +1300

    Updating image service to return smaller payloads and include metaCategory

--- backend/src/api/image/image.service.ts
+++ backend/src/api/image/image.service.ts
@@ -6,6 +6,10 @@ import { FileUploadDTO, FileUploadErrorDTO } from './interface/fileUpload.dto';
 import { ImageDTO } from './interface/image.dto';
 import { CategoryService } from '../category/category.service';
 import { OutfitService } from '../outfit/outfit.service';
+import {
+  CategoryDTO,
+  MetaCategoryName,
+} from '../category/interfaces/category.dto';
 
 @Injectable()
 export class ImageService {
@@ -146,17 +150,28 @@ export class ImageService {
     return responseData;
   }
 
-  async getImagesByUserId(userId: string): Promise<{data, error}> {
-
+  async getImagesByUserId(userId: string): Promise<{ data; error }> {
     // Check userId is null
-    if (!userId)
-    {
-      return {data: null, error: "Invalid userId"};
+    if (!userId) {
+      return { data: null, error: 'Invalid userId' };
     }
     const rows = await this.supa
       .getClient()
       .from('Image')
-      .select('*')
+      .select(
+        `
+        imageId,
+        processedUrl,
+        ImageCategory (
+          imageCategoryId,
+          categoryId,
+          ItemCategory (
+            name,
+            metaCategory
+          )
+        )
+        `,
+      )
       .eq('userId', userId);
     if (rows.error) {
       return { data: null, error: rows.error };
@@ -164,19 +179,25 @@ export class ImageService {
 
     let images = Array<ImageDTO>();
 
-    for (const image of rows.data) {
-      const categories = (
-        await this.categoryService.getImageCategoriesByImageId(image.imageId)
-      ).data;
-      images.push({
-        imageId: image.imageId,
-        created_at: image.created_at,
-        rawUrl: image.rawUrl,
-        processedUrl: image.processedUrl,
-        userId: image.userId,
-        categories: categories,
+    rows.data.forEach((item) => {
+      const categories = [];
+      item.ImageCategory.forEach((category) => {
+        const newCat: CategoryDTO = {
+          categoryId: category.categoryId,
+          name: category['ItemCategory']['name'],
+          metaCategoryName: category['ItemCategory']['metaCategory'],
+        };
+        categories.push(newCat);
       });
-    }
+
+      const image: ImageDTO = {
+        imageId: item.imageId,
+        processedUrl: item.processedUrl,
+        categories: categories,
+      };
+      images.push(image);
+    });
+
     return { data: images, error: rows.error };
   }
 
@@ -252,80 +273,76 @@ export class ImageService {
     };
   }
 
-    async searchImageByKeyWord(keyword: string, userId: string): Promise<{data, error}> {
-      //Check userId is null
-      if (!userId)
-      {
-        return {data: null, error: "Invalid userId"};
-      }
-
-      
-      let searchResult = new Array<ImageDTO>();
-
-      // Get all clothing items of the user   
-      let queryImagesResult = await this.getImagesByUserId(userId);
-
-      // Check if the database query gets error
-      if (!queryImagesResult.data){
-          return {data: null, error: queryImagesResult.error };
-      }
-
-      const allUserimages = queryImagesResult.data;
-
-      // Check if keyword = null
-      if (keyword == null) {
-        return { data: allUserimages, error: null };
-      }
-
-      for (const image of allUserimages) {
-        // Check if any category name matches the keyword
-        let hasKeyword = false;
-        for (const cat of image.categories) {
-          if (cat.name.toLowerCase().includes(keyword.toLowerCase())) {
-              hasKeyword = true;
-              break;
-          }
-          }
-          if (hasKeyword) {
-            searchResult.push(image);
-          }
-        }
-        
-        return {data: searchResult, error: null};
-      
+  async searchImageByKeyWord(
+    keyword: string,
+    userId: string,
+  ): Promise<{ data; error }> {
+    //Check userId is null
+    if (!userId) {
+      return { data: null, error: 'Invalid userId' };
     }
+
+    let searchResult = new Array<ImageDTO>();
+
+    // Get all clothing items of the user
+    let queryImagesResult = await this.getImagesByUserId(userId);
+
+    // Check if the database query gets error
+    if (!queryImagesResult.data) {
+      return { data: null, error: queryImagesResult.error };
+    }
+
+    const allUserimages = queryImagesResult.data;
+
+    // Check if keyword = null
+    if (keyword == null) {
+      return { data: allUserimages, error: null };
+    }
+
+    for (const image of allUserimages) {
+      // Check if any category name matches the keyword
+      let hasKeyword = false;
+      for (const cat of image.categories) {
+        if (cat.name.toLowerCase().includes(keyword.toLowerCase())) {
+          hasKeyword = true;
+          break;
+        }
+      }
+      if (hasKeyword) {
+        searchResult.push(image);
+      }
+    }
+
+    return { data: searchResult, error: null };
+  }
 }
 
+//   async filterImageByCategory(
+//     categories: Array<number>,
+//     userId,
+//   ): Promise<Array<Image>> {
+//     let filterResult = new Array<Image>();
 
-  //   async filterImageByCategory(
-  //     categories: Array<number>,
-  //     userId,
-  //   ): Promise<Array<Image>> {
-  //     let filterResult = new Array<Image>();
+//     const allUserimages = await this.getImagesByUserId(userId);
 
-  //     const allUserimages = await this.getImagesByUserId(userId);
+//     console.log(allUserimages);
 
-  //     console.log(allUserimages);
+//     // Convert the array of numbers to an array of BigInt for comparison
+//     const bigIntCategories = categories.map((cat) => BigInt(cat));
 
-  //     // Convert the array of numbers to an array of BigInt for comparison
-  //     const bigIntCategories = categories.map((cat) => BigInt(cat));
-
-  //     // Iterate over all user images
-  //     for (const image of allUserimages) {
-  //       // Check if any category id of the image matches the provided categories
-  //       let hasCategory = false;
-  //       for (const cat of image.categories) {
-  //         if (categories.includes(Number(cat.categoryID))) {
-  //           hasCategory = true;
-  //           break;
-  //         }
-  //       }
-  //       if (hasCategory) {
-  //         filterResult.push(image);
-  //       }
-  //     }
-  //     return filterResult;
-  //   }
-
-
-
+//     // Iterate over all user images
+//     for (const image of allUserimages) {
+//       // Check if any category id of the image matches the provided categories
+//       let hasCategory = false;
+//       for (const cat of image.categories) {
+//         if (categories.includes(Number(cat.categoryID))) {
+//           hasCategory = true;
+//           break;
+//         }
+//       }
+//       if (hasCategory) {
+//         filterResult.push(image);
+//       }
+//     }
+//     return filterResult;
+//   }
