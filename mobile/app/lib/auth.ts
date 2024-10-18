2edccfdafea4920e93ad85a16524b049546206c4
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2024-10-18 17:36:45 +1300

    Formatting auth and removing print statement

--- mobile/app/lib/auth.ts
+++ mobile/app/lib/auth.ts
@@ -1,40 +1,36 @@
-import * as SecureStore from 'expo-secure-store';
+import * as SecureStore from "expo-secure-store";
 
 interface User {
-  userId: string;
-  email: string;
-  accessToken: string;
+	userId: string;
+	email: string;
+	accessToken: string;
 }
 
 export async function loginUser(userData: User): Promise<void> {
-    await SecureStore.setItemAsync("userId", userData.userId);
-    await SecureStore.setItemAsync("email", userData.email);
-    await SecureStore.setItemAsync("accessToken", userData.accessToken);
+	await SecureStore.setItemAsync("userId", userData.userId);
+	await SecureStore.setItemAsync("email", userData.email);
+	await SecureStore.setItemAsync("accessToken", userData.accessToken);
 }
 
 export function getUser(): User | null {
-    const userId: string | null = SecureStore.getItem("userId");
-    if (userId == null) {
-        return null;
-    }
-    const email: string | null = SecureStore.getItem("email");
-    if (email == null) {
-        return null;
-    }
-    const accessToken: string | null = SecureStore.getItem("accessToken");
-    if (accessToken == null) {
-        return null;
-    }
+	const userId: string | null = SecureStore.getItem("userId");
+	if (userId == null) {
+		return null;
+	}
+	const email: string | null = SecureStore.getItem("email");
+	if (email == null) {
+		return null;
+	}
+	const accessToken: string | null = SecureStore.getItem("accessToken");
+	if (accessToken == null) {
+		return null;
+	}
 
-    console.log(userId);
-    console.log(email);
-    console.log(accessToken);
+	const userData: User = {
+		userId: userId,
+		email: email,
+		accessToken: accessToken,
+	};
 
-    const userData: User = {
-        userId: userId,
-        email: email,
-        accessToken: accessToken
-    }
-    
-    return userData;
+	return userData;
 }
