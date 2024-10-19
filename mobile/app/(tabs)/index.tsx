0bc1c7774070a85a501b4170f805b4ab23efea7f
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2024-10-19 18:55:48 +1300

    Making sure cancelling steps out of the category secletion pane

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -438,6 +438,7 @@ export default function HomePage() {
 										<Button
 											title="Cancel"
 											onPress={() => {
+												setIsCategorySelectionVisible(false);
 												setIsModalVisible(false);
 											}}
 										/>
